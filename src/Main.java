7ba292481bf066f41ce48a3ff32f7b9ec35a3610
Sameer <bs.sameer1@gmail.com>
2016-10-15 20:36:44 -0400

    Split input param

--- src/Main.java
+++ src/Main.java
@@ -7,11 +7,12 @@ public class Main {
         String PUB_FILE = args[0];
         String policy = args[1];
         String inputfile = args[2];
+        String encfile = args[3];
 
         Cpabe encryptor = new Cpabe();
 
         try {
-            encryptor.enc(PUB_FILE, policy, inputfile, inputfile);
+            encryptor.enc(PUB_FILE, policy, inputfile, encfile);
         } catch(Exception e) {
             System.err.println(e);
         }
